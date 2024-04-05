c7f8625ddd89ee2e680f2cc371b1e3ee765498ef
Oatkrub <sirawit.v@gmail.com>
2024-04-05 15:38:55 +0700

    resize booking item to fit

--- src/component/BookingItem.tsx
+++ src/component/BookingItem.tsx
@@ -8,7 +8,7 @@ interface BookingItemProps {
 const BookingItem: React.FC<BookingItemProps> = ({ image, title }) => {
     return (
         <div className="bg-white shadow-lg rounded-md overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
-            <img className="w-30 h-30 object-cover object-center px-10 py-10" src={image} alt={title} />
+            <img className="w-full h-auto object-cover px-10 py-10" src={image} alt={title} />
             <div className="p-4">
                 <h2 className="text-gray-800 text-lg font-semibold mb-2">{title}</h2>
                 <button className="rounded-2xl bg-[green] text-white p-2">
